9b96678be0baa227fd1da2de6e26e377d9d56a88
Alex Berry <berry3@indiana.edu>
2016-12-07 23:59:43 -0500

    added some code testing embedded sets in ODocuments

--- src/main/java/scratchpad/OrientDbExample.kt
+++ src/main/java/scratchpad/OrientDbExample.kt
@@ -1,11 +1,16 @@
 package scratchpad
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
+import com.orientechnologies.orient.core.metadata.schema.OType
 import com.orientechnologies.orient.core.record.impl.ODocument
+import com.orientechnologies.orient.core.sql.OCommandSQL
 
 fun orientDB() {
     val db = ODatabaseDocumentTx("memory:test")
     db.create<ODatabaseDocumentTx>()
+
+    val testClass = db.metadata.schema.createClass("Test")
+    testClass.createProperty("set", OType.EMBEDDEDSET)
 
     val doc = ODocument("Test")
     doc.field("test", "Test")
@@ -16,10 +21,16 @@ fun orientDB() {
     assert(db.browseClass("Test").toList().size == 1)
 
     val doc2 = db.browseClass("Test").toList().first()
-
     val result = doc2.field<Set<String>>("set")
-
+    assert(result.size == 3)
     println(result)
+
+    val i = db.command(OCommandSQL("update Test add set = \"test4\"")).execute<Int>()
+    assert(i == 1)
+    val doc3 = db.browseClass("Test").toList().first()
+    val result3 = doc3.field<Set<String>>("set")
+    (result3.size == 4)
+    println(result3)
 
     db.close()
 }
