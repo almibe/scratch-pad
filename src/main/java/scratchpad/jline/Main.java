486e2b026dbdf773aaff1994382b44ae867eb598
Alex Berry <berry3@indiana.edu>
2018-07-20 17:39:12 -0400

    basic jline test

--- src/main/java/scratchpad/jline/Main.java
+++ src/main/java/scratchpad/jline/Main.java
@@ -1,4 +1,23 @@
 package scratchpad.jline;
 
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Test");
+        final Terminal terminal = TerminalBuilder.terminal();
+
+        //https://github.com/jline/jline3/wiki/LineReader
+        final LineReader lineReader = LineReaderBuilder.builder()
+            .terminal(terminal)
+            .build();
+
+        String name = lineReader.readLine("Enter name:");
+        String pw = lineReader.readLine("Enter password:", '*');
+        terminal.close();
+        System.out.println(name + " - " + pw);
+    }
 }
